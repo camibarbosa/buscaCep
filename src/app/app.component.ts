434a38b35bf96beca43cf46acc561b877f226867
Camila <97048604+camibarbosa@users.noreply.github.com>
2023-05-23 15:56:19 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,64 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'buscaCep';
+
+  constructor(private http: HttpClient){}
+
+  //forms variables
+  cep: string = '';
+
+  //variaveis de retorno
+  endereco : any;
+  retorno = {
+    rua : '',
+    cidade : '',
+    estado : ''
+  }
+
+  //logadouro, localidade= cidade, uf=estado
+  foiPesquisado = false;
+  emEspera = false;
+
+  pesquisa(){
+
+    if (!this.cep) {
+      // exibir uma mensagem para o usuário informando que o campo está vazio
+     alert('Campo de pesquisa vazio. Insira um CEP válido.');
+      return; // Sai do método sem prosseguir com a pesquisa
+    }
+
+    this.emEspera = true;
+    this.cep = this.cep.replace(/\D/g, '');
+
+    const url = 'http://viacep.com.br/ws/' + this.cep + '/json/';
+
+    //while (true) teste = 0;
+    this.http.get(url).subscribe(
+      (res) => {
+        this.endereco = res;
+        this.retorno.rua = this.endereco.logradouro;
+        this.retorno.cidade = this.endereco.localidade;
+        this.retorno.estado = this.endereco.uf;
+      },
+      (error) => {  
+        // Tratamento de erro
+        console.error('Ocorreu um erro na requisição:', error);
+      
+      }
+    );
+
+    this.foiPesquisado = true;
+    this.emEspera = false;
+  }
+
+  limpa(){
+    this.foiPesquisado = false;
+    this.emEspera = false;
+    this.cep = "";
+    this.endereco = "";
+    this.retorno.rua = "";
+    this.retorno.cidade = "";
+    this.retorno.estado = "";
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
